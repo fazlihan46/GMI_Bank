9a7d2e82a7d6d0ad40673460b7b46f79a795233e
Kerim5858 <kerimanulur@gmail.com>
2020-11-09 11:38:39 -0500

    11/09 first

--- src/test/java/gmibank_team06/utilities/Driver.java
+++ src/test/java/gmibank_team06/utilities/Driver.java
@@ -40,7 +40,7 @@ public class Driver {
                     driver = new InternetExplorerDriver();
                     break;
                 case "safari":
-                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    //WebDriverManager.getInstance(SafariDriver.class).setup();
                     driver = new SafariDriver();
                     break;
                 case "chrome-headless":
